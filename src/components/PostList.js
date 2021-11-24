cee302b4e2383b9607de585d56dbced2f8f1f912
Chethanck <45870776+Chethanck@users.noreply.github.com>
2021-11-24 23:11:11 +0530

    Post List page addition

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PostList extends Component {
+    render() {
+        return (
+            <div>
+                PostList88888888888888888888888888
+            </div>
+        )
+    }
+}
+
+export default PostList
