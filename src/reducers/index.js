4a7ea272b3d2afca0980b1bcb68ff03544582d68
Chethanck <45870776+Chethanck@users.noreply.github.com>
2021-12-01 22:36:25 +0530

    Reducers return value and rules

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
+import postReducer from './postReducer';
+
 
 export default combineReducers({
-    replaceMe: () => "Hi There"//Dummy reducers to remove the console error
+    posts: postReducer
 });
---
+++ src/reducers/postReducer.js
@@ -0,0 +1,5 @@
+const postReducer = () => {
+    return 123;
+};// Reducers should not return undefined anywhere
+
+export default postReducer;
