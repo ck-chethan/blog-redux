7ad3050d86b9b5f9712a24ac640a8e3bcd534677
Chethanck <45870776+Chethanck@users.noreply.github.com>
2021-12-01 15:15:41 +0530

    Usage of Redux thunk Refractored

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,10 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-    return async function (distpatch, getState) {
-        const response = await jsonPlaceholder.get('/posts');
+export const fetchPosts = () => async distpatch => {
+    const response = await jsonPlaceholder.get('/posts');
 
-        distpatch({
-            type: 'FETCH_POSTS',
-            payload: response
-        });
-    };
+    distpatch({
+        type: 'FETCH_POSTS',
+        payload: response
+    });
 };
